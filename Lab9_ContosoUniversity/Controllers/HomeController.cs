63d7c51ced6f8e1683aa3194ae83ac6ec30f9338
shamseen <shamseen.c@gmail.com>
2015-08-10 16:47:00 -0400

    grouping students by date and showing date/count

--- Lab9_ContosoUniversity/Controllers/HomeController.cs
+++ Lab9_ContosoUniversity/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Lab9_ContosoUniversity.DAL;
+using Lab9_ContosoUniversity.ViewModels;
 
 namespace Lab9_ContosoUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private SchoolContext db = new SchoolContext();
         public ActionResult Index()
         {
             return View();
@@ -15,9 +18,16 @@ namespace Lab9_ContosoUniversity.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            //for every unique enrollment date, create a new collection dategroup
+            //then create a new instance of EnrollmentDateGroup and pass in the date and number of students 
+            IQueryable<EnrollmentDateGroup> data = from student in db.Students
+                                                    group student by student.EnrollmentDate into dategroup
+                                                    select new EnrollmentDateGroup()
+                                                    {
+                                                        EnrollmentDate = dategroup.Key, //the actual date
+                                                        StudentCount = dategroup.Count() //num of students
+                                                    };
+            return View(data.ToList());
         }
 
         public ActionResult Contact()
