e48bdf5aedbc6156c4668ead7a7fe38cbcf852e2
shamseen <shamseen.c@gmail.com>
2015-08-12 14:28:29 -0400

    instructor, department, officeAssignment models

--- Lab9_ContosoUniversity/Models/Course.cs
+++ Lab9_ContosoUniversity/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Lab9_ContosoUniversity.Models
@@ -7,14 +8,23 @@ namespace Lab9_ContosoUniversity.Models
     public class Course
     {
         //how to generate properties for a database
-        // the .None means I can input my own values instead of autogenerating
+        //the .None means I can input my own values instead of autogenerating
         //good because course id's usually have a certain code
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name ="Number")]
         public int CourseID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
+
+        [Range(0, 5)]
         public int Credits { get; set; }
 
-        //one-to-many
+        public int DepartmentID { get; set; }
+
+        //nav properties
+        public virtual Department Department { get; set; } //many-to-one
+        public virtual ICollection<Instructor> Instructors { get; set; } //one-to-many
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
 }
---
+++ Lab9_ContosoUniversity/Models/Department.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab9_ContosoUniversity.Models
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+
+        [StringLength(50, MinimumLength =3)]
+        public string Name { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName ="money")]
+        public decimal Budget { get; set; }
+
+        //foreign key
+        public int? InstructorID { get; set; }
+
+        //nav properties
+        public virtual Instructor Administrator { get; set; } //one to one
+        public virtual ICollection<Course> Courses { get; set; } //one to many
+    }
+}
