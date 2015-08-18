f68aec4c4d30faac5aaf459ac0b6481c70dcbdf8
shamseen <shamseen.c@gmail.com>
2015-08-18 13:38:01 -0400

    adding dbsets and join tables for new models

--- Lab9_ContosoUniversity/DAL/SchoolContext.cs
+++ Lab9_ContosoUniversity/DAL/SchoolContext.cs
@@ -19,12 +19,25 @@ namespace Lab9_ContosoUniversity.DAL
         public DbSet<Student> Students { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Course> Courses { get; set; }
+        public DbSet<Department> Departments {get;set;}
+        public DbSet<Instructor> Instructors {get;set;}
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
 
-        //prevents table names from being pluralized so Student instead of Students
-        //really it's a matter of preference but whatever. I don't wanna look out for name changes throughout tutorial
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            //prevents table names from being pluralized so Student instead of Students
+            //really it's a matter of preference but whatever. I don't wanna look out for name changes throughout tutorial
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            //making a join table bc instructor and course are many-to-many
+            //mapping table to have a courseID column on the left, and instructorID column on the right.
+            //then naming table CourseInstructor
+            modelBuilder.Entity<Course>()
+                .HasMany(c => c.Instructors).WithMany(i => i.Courses)
+                .Map(t => t.MapLeftKey("CourseID")
+                            .MapRightKey("InstructorID")
+                            .ToTable("CourseInstructor"));
         }
     }
 }
